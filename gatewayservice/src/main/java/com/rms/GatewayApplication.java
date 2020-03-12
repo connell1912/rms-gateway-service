5d21d3685bb32fdf95ae859a0d29d9351870fc8a
aragasa <58983597+aragasa@users.noreply.github.com>
2020-03-11 22:08:59 -0400

    Update GatewayApplication.java
    Documentation added

--- gatewayservice/src/main/java/com/rms/GatewayApplication.java
+++ gatewayservice/src/main/java/com/rms/GatewayApplication.java
@@ -5,6 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+/**
+*@author 1912dec16 Fullstack Java
+*<br>
+*<br>
+*The Zuul gateway gives us a location to access all of the services within the app by creating routes to each
+*service. It works in conjunction with the Eureka Server which gives Zuul the locations of all the other microservices.
+*/
 @SpringBootApplication
 @EnableZuulProxy
 @EnableEurekaClient
